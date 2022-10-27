855619075000bec04fe87442f8159adaac893de0
IldarG <Bumble_bee91@mail.ru>
2022-10-27 17:32:55 +0300

    Task23

---
+++ Task23/Program.cs
@@ -0,0 +1,27 @@
+﻿/* Задача 23
+Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+5 ->
+1 | 1
+2 | 8
+3 | 27
+4 | 64
+5 | 125 */
+
+void Table(int digit)
+{
+    if (digit > 0)
+    {
+      int count = 1;
+      while (count <= digit)
+      {
+        Console.WriteLine($"{count,4} {count*count*count,4}");
+        count ++;
+      }
+    }
+    else Console.WriteLine("Некорректный ввод");
+}
+
+Console.WriteLine("Введите натуральное число: ");
+int digit = Convert.ToInt32(Console.ReadLine());
+
+Table(digit);
